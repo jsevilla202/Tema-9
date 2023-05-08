64b46d003a194122b33c3f4720fb5f5bb54bd418
jsevilla202 <jsevilla@iesnervion.es>
2023-05-08 10:45:18 +0200

    Ejercicio 5 base

---
+++ Tema 9/src/ejercicio5/Main.java
@@ -0,0 +1,13 @@
+package ejercicio5;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Map<String, Integer> productos = new TreeMap<>();
+
+	}
+
+}
